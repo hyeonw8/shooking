aaca861a496ba08a14f497e7a496caf98c03c056
hyeonw8 <whgusrud223@gmail.com>
2026-02-14 01:53:31 +0900

    feat: 상품 목록 상단 정보 영역 컴포넌트 추가
    - 상품 목록 페이지 헤더 및 전체 상품 수 표시 기능 추가

---
+++ src/features/product/ProductListHeader.jsx
@@ -0,0 +1,10 @@
+import { mockProducts } from "../../mocks/product";
+
+export const ProductListHeader = () => {
+  return (
+    <div className="flex flex-col gap-y-1 py-5">
+      <h1 className="text-3xl font-bold">신발 상품 목록</h1>
+      <p className="text-md">현재 {mockProducts.length}개의 상품이 있습니다.</p>
+    </div>
+  );
+};
