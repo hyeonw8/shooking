5f2404b30465ddaab21faf685b724eaf00bc88de
hyeonw8 <whgusrud223@gmail.com>
2026-02-14 02:03:29 +0900

    feat: 상품 목록 카드 및 그리드 컴포넌트 구현
    - ProductList 그리드 레이아웃 구현
    - ProductCard UI 구성

---
+++ src/features/product/ProductList.jsx
@@ -0,0 +1,12 @@
+import { mockProducts } from "../../mocks/product";
+import { ProductCard } from "./ProductCard";
+
+export const ProductList = () => {
+  return (
+    <div className="grid grid-cols-2 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+      {mockProducts.map((item) => (
+        <ProductCard item={item} key={item.id} />
+      ))}
+    </div>
+  );
+};
