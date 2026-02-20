665562d064d2d9998974a08801cc6029acbc9955
hyeonw8 <whgusrud223@gmail.com>
2026-02-21 00:45:21 +0900

    chore: simple-import-sort 설정 추가 및 설정 파일 정리
    - ESLint에 import 정렬 규칙 적용
    - Vite config import 구조 정리

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,12 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from "eslint-plugin-storybook";
 
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
 
 export default defineConfig([globalIgnores(['dist']), {
   files: ['**/*.{js,jsx}'],
@@ -23,7 +24,13 @@ export default defineConfig([globalIgnores(['dist']), {
       sourceType: 'module',
     },
   },
+    plugins: {
+      'simple-import-sort': eslintPluginSimpleImportSort,
+    },
+
   rules: {
     'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
   },
-}, ...storybook.configs["flat/recommended"]])
+}, ...storybook.configs["flat/recommended"]]);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 /// <reference types="vitest/config" />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 import path from 'node:path';
