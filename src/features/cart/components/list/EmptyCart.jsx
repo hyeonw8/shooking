2576bdad724d8f38a7ed77228bd4202fc3b33858
hyeonw8 <whgusrud223@gmail.com>
2026-03-25 03:09:51 +0900

    style: PageHeaderInfo 텍스트 처리 및 EmptyCart 반응형 UI 개선

--- src/features/cart/components/list/EmptyCart.jsx
+++ src/features/cart/components/list/EmptyCart.jsx
@@ -10,19 +10,21 @@ export const EmptyCart = () => {
   return (
     <section className="flex min-h-[60vh] flex-col items-center justify-center text-center">
       <div className="py-3">
-        <span className="text-4xl">🛒</span>
+        <span className="text-3xl lg:text-4xl">🛒</span>
       </div>
 
-      <h2 className="text-lg font-semibold text-black">장바구니가 비었어요!</h2>
+      <h2 className="text-lg font-semibold text-black lg:text-xl">
+        장바구니가 비었어요!
+      </h2>
 
-      <p className="mt-1 text-sm leading-6 text-gray-600">
+      <p className="mt-1 text-sm leading-6 text-gray-600 lg:text-base">
         원하는 상품을 장바구니에 담아 보세요.
       </p>
 
       <button
         type="button"
         onClick={handleMoveToProducts}
-        className="mt-14 rounded-full bg-black px-7 py-3 text-sm font-semibold text-white transition hover:opacity-80"
+        className="mt-14 min-w-[200px] cursor-pointer rounded-full bg-black px-7 py-3 text-sm font-semibold text-white transition hover:opacity-80 lg:text-base lg:min-w-[220px]"
       >
         상품 보러가기
       </button>
