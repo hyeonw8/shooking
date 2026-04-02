360aac29a18b30f4ba903b977491b29f03591be8
hyeonw8 <whgusrud223@gmail.com>
2026-04-03 02:50:12 +0900

    refactor: PaymentsProvider 제거 및 PaymentsPage 기반 라우팅 구조 정리
    - PaymentsProvider 제거하고 Recoil 기반 상태 관리로 통합
    - MyCardsPage 대신 PaymentsPage를 결제 페이지로 연결하도록 수정
    - 라우팅 순서 및 구조 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
 import { Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 
-import { PaymentsProvider } from './features/payments/shared/PaymentsProvider';
 import AddCardPage from './pages/AddCardPage';
 import CartPage from './pages/CartPage';
 import Home from './pages/Home';
-import MyCardsPage from './pages/MyCardsPage';
+import PaymentsPage from './pages/PaymentsPage';
 import PaymentSuccessPage from './pages/PaymentSuccessPage';
 import ProductDetailPage from './pages/ProductDetailPage';
 
 export default function App() {
   return (
     <RecoilRoot>
-      <PaymentsProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/payments" element={<MyCardsPage />} />
-          <Route path="/payments/new" element={<AddCardPage />} />
-          <Route path="/payments/success" element={<PaymentSuccessPage />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/products/:id" element={<ProductDetailPage />} />
-        </Routes>
-      </PaymentsProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products/:id" element={<ProductDetailPage />} />
+        <Route path="/payments" element={<PaymentsPage />} />
+        <Route path="/payments/new" element={<AddCardPage />} />
+        <Route path="/payments/success" element={<PaymentSuccessPage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
     </RecoilRoot>
   );
 }
